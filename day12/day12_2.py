934d2d8eafd58a853cd1900ec2de8b59e17b1313
A.M <box313@gmail.com>
2021-12-12 23:32:06 +0300

    Day 12 solutions

---
+++ day12/day12_2.py
@@ -0,0 +1,33 @@
+import sys
+
+
+def is_big_node(node_name):
+    return all(c >= "A" and c <= "Z" for c in node_name)
+
+
+cave_map = {}
+for line in sys.stdin:
+    n1, n2 = line.strip().split("-")
+    if n2 != "start":
+        cave_map.setdefault(n1, []).append(n2)
+    if n1 != "start":
+        cave_map.setdefault(n2, []).append(n1)
+
+current_paths = [(["start"], False)]
+paths = []
+while current_paths:
+    new_current_paths = []
+    for current_path, has_double in current_paths:
+        last_node = current_path[-1]
+        for next_node in cave_map[last_node]:
+            if next_node == "end":
+                paths.append(current_path + [next_node])
+            elif is_big_node(next_node):
+                new_current_paths.append((current_path + [next_node], has_double))
+            elif next_node not in current_path:
+                new_current_paths.append((current_path + [next_node], has_double))
+            elif not has_double:
+                new_current_paths.append((current_path + [next_node], True))
+    current_paths = new_current_paths
+
+print(len(paths))
